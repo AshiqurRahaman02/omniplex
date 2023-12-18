e79f49f75d8db56fd22c606adf1a385ffd155c7c
AshiqurRahaman02 <ashiqur999999@gamil.com>
2023-12-18 19:52:37 +0530

    logo icon updated

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// pages
+import Home from './pages/Home';
+import Sign from './pages/Sign';
+import Explore from './pages/Explore';
+import About from './pages/About';
+import Account from './pages/Account';
+
+// components
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Router>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign" element={<Sign />} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/account" element={<Account />} />
+        <Route path="explore" element={<Explore/>} />
+      </Routes>
+      <Footer/>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div>
+      It's nav bar
+    </div>
+  )
+}
+
+export default Nav
