9da93db8e574954f7337e8e5f86f0cafe4f6edad
AshiqurRahaman02 <ashiqur999999@gamil.com>
2023-12-19 11:20:01 +0530

    nav section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+import "./App.css"
+
 // pages
 import Home from './pages/Home';
 import Sign from './pages/Sign';
