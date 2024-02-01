c9efe375aa393f0367de03f4d08c9563bb2d1221
AshiqurRahaman02 <ashiqur999999@gamil.com>
2024-02-01 22:57:34 +0530

    chat container added

--- src/pages/collabcraft/Home.jsx
+++ src/pages/collabcraft/Home.jsx
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Nav from "../../components/omniplex/Nav";
 import Footer from "../../components/omniplex/Footer";
+
+import { ToastContainer, toast } from "react-toastify";
+
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { roomRoutes } from "../../routes/collabcraft.route";
 
 const rooms = [
 	{
@@ -42,11 +49,178 @@ const rooms = [
 	},
 ];
 
-function Home() {
-	const [roomName, setRoomName] = useState("");
-	const [password, setPassword] = useState("");
+const notify = (message = "done", type = "success") => {
+	if (type === "error") {
+		toast.error(message, {
+			position: "top-right",
+			autoClose: 5000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "light",
+		});
+	} else if (type === "success") {
+		toast.success(message, {
+			position: "top-right",
+			autoClose: 5000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "light",
+		});
+	} else if (type === "info") {
+		toast.info(message, {
+			position: "top-right",
+			autoClose: 5000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "light",
+		});
+	} else if (type === "warning") {
+		toast.warn(message, {
+			position: "top-right",
+			autoClose: 5000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "light",
+		});
+	} else {
+		toast("🦄 Wow so easy!", {
+			position: "top-right",
+			autoClose: 5000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "light",
+		});
+	}
+};
 
+function Home() {
+	const navigate = useNavigate();
 	const [joinRoom, setJoinRoom] = useState(true);
+	const [userDetails, setUserDetails] = useState();
+	const [token, setToken] = useState();
+
+	const [userRooms, setUserRooms] = useState(rooms);
+	const [roomName, setRoomName] = useState("");
+	const [roomPassword, setRoomPassword] = useState("");
+
+	useEffect(() => {
+		document.title = "Collabcraft | Home";
+		const userDetails = localStorage.getItem("userInfo");
+		const token = localStorage.getItem("token");
+		if (token && userDetails) {
+			const parsedUserDetails = JSON.parse(userDetails);
+			setUserDetails(parsedUserDetails);
+			setToken(token);
+
+			getUserRooms(token);
+		} else {
+			let message = "Please Login first.";
+			notify(message, "error");
+			setTimeout(() => {
+				navigate("/sign");
+			}, 3000);
+		}
+	}, []);
+
+	const getUserRooms = (token) => {
+		fetch(`${roomRoutes.getUserRooms}`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: token,
+			},
+		})
+			.then((res) => res.json())
+			.then((res) => {
+				if (res.isError) {
+					notify(res.message, "warning");
+				} else {
+					setUserRooms(res.rooms);
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+				notify(err.message, "error");
+			});
+	};
+
+	const handelJoinRoom = () => {
+		if (!roomName) {
+			notify("Please enter room id", "warning");
+			return;
+		}
+		if (!roomPassword) {
+			notify("Please enter room password", "warning");
+			return;
+		}
+		console.log(token);
+		fetch(`${roomRoutes.joinRoom}`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: token,
+			},
+			body: JSON.stringify({ roomId: roomName, password: roomPassword }),
+		})
+			.then((res) => res.json())
+			.then((res) => {
+				if (res.isError) {
+					notify(res.message, "warning");
+				} else {
+					navigate(`/collabcraft/room/${res.room._id}`);
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+				notify(err.message, "error");
+			});
+	};
+
+	const handelCreateRoom = () => {
+		if (!roomName) {
+			notify("Please enter room name", "warning");
+			return;
+		}
+		if (!roomPassword) {
+			notify("Please enter room password", "warning");
+			return;
+		}
+		fetch(`${roomRoutes.createRoom}`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: token,
+			},
+			body: JSON.stringify({ name: roomName, password: roomPassword }),
+		})
+			.then((res) => res.json())
+			.then((res) => {
+				if (res.isError) {
+					notify(res.message, "warning");
+				} else {
+					navigate(`/collabcraft/room/${res.room._id}`);
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+				notify(err.message, "error");
+			});
+	};
 	return (
 		<div id="collabcraft">
 			<Nav title="Collabcraft" />
@@ -58,7 +232,14 @@ function Home() {
 							<div>
 								<h2>{room.roomName}</h2>
 								<p>Current Users: {room.currentUsers}</p>
-								<button>Join Now </button>
+								<button
+									onClick={() =>
+										navigate(`/collabcraft/room/${room.id}`)
+									}
+								>
+									<span>Join Now </span>
+									<FontAwesomeIcon icon={faArrowRight} />
+								</button>
 							</div>
 						))}
 					</div>
@@ -75,7 +256,7 @@ function Home() {
 									? "0"
 									: "1px solid rgb(212, 212, 212)",
 							}}
-							onClick={()=>setJoinRoom(true)}
+							onClick={() => setJoinRoom(true)}
 						>
 							Join Room
 						</span>
@@ -88,44 +269,46 @@ function Home() {
 									? "0"
 									: "1px solid rgb(212, 212, 212)",
 							}}
-
-							onClick={()=>setJoinRoom(false)}
+							onClick={() => setJoinRoom(false)}
 						>
 							Create Room
 						</span>
 					</div>
 					<div>
-					{joinRoom ? (
-						<h2>Join Room</h2>
-					) : (
-						<h2>Create Room</h2>
-					)}
-						<div className="form-control">
-							<input
-								type="text"
-								required
-								value={roomName}
-								onChange={(e) => setRoomName(e.target.value)}
-							/>
-							<label>Enter Room Name</label>
-						</div>
-						<div className="form-control">
-							<input
-								type="password"
-								required
-								value={password}
-								onChange={(e) => setPassword(e.target.value)}
-							/>
-							<label>Enter Password</label>
-						</div>
+						{joinRoom ? <h2>Join Room</h2> : <h2>Create Room</h2>}
+						<>
+							<div className="form-control">
+								<input
+									type="text"
+									required
+									value={roomName}
+									onChange={(e) => setRoomName(e.target.value)}
+								/>
+								<label>Enter Room {joinRoom ? "Id" : "Name"}</label>
+							</div>
+							<div className="form-control">
+								<input
+									type="password"
+									required
+									value={roomPassword}
+									onChange={(e) => setRoomPassword(e.target.value)}
+								/>
+								<label>Enter Room Password</label>
+							</div>
+						</>
 					</div>
 					{joinRoom ? (
-						<button>Join Room</button>
+						<button id="join-create-room" onClick={handelJoinRoom}>
+							Join Room
+						</button>
 					) : (
-						<button>Create Room</button>
+						<button id="join-create-room" onClick={handelCreateRoom}>
+							Create Room
+						</button>
 					)}
 				</div>
 			</div>
+			<ToastContainer />
 			{/* <Footer /> */}
 		</div>
 	);
--- src/pages/collabcraft/Room.jsx
+++ src/pages/collabcraft/Room.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CodeEditor from "../../components/collabcraft/CodeEditor";
 import Canvas from "../../components/collabcraft/Canvas";
 
 import "../../styles/collabcraft.css";
 import Users from "../../components/collabcraft/Users";
+
+import { ToastContainer, toast } from "react-toastify";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -16,10 +18,15 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import {
 	faCircleStop,
+	faCircleXmark,
 	faComment,
 	faMoon,
+	faPaperPlane,
 	faSun,
 } from "@fortawesome/free-regular-svg-icons";
+import { useNavigate, useParams } from "react-router-dom";
+import { roomRoutes } from "../../routes/collabcraft.route";
+import Draggable from "react-draggable";
 
 const users = [
 	{
@@ -76,15 +83,157 @@ const users = [
 	},
 ];
 
+const notify = (message = "done", type = "success") => {
+	if (type === "error") {
+		toast.error(message, {
+			position: "top-right",
+			autoClose: 5000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "light",
+		});
+	} else if (type === "success") {
+		toast.success(message, {
+			position: "top-right",
+			autoClose: 5000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "light",
+		});
+	} else if (type === "info") {
+		toast.info(message, {
+			position: "top-right",
+			autoClose: 5000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "light",
+		});
+	} else if (type === "warning") {
+		toast.warn(message, {
+			position: "top-right",
+			autoClose: 5000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "light",
+		});
+	} else {
+		toast("🦄 Wow so easy!", {
+			position: "top-right",
+			autoClose: 5000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "light",
+		});
+	}
+};
+
 function Room() {
-	const [userDetails, setUserDetails] = useState({
+	const { roomId } = useParams();
+	const navigate = useNavigate();
+	const [userState, setUserState] = useState({
 		audio: false,
 		video: false,
 		displayCode: false,
 		darkMode: true,
 	});
-
+	const [roomChats, setRoomChats] = useState([
+		{
+			userId: "a;ldkjf",
+			message: "Room",
+			time: new Date(),
+			userName: "a;lkdjffhkjsfg",
+		},
+	]);
 	const [isMaximized, setIsMaximized] = useState(false);
+	const [displayChats, setDisplayChats] = useState(false);
+	const [userDetails, setUserDetails] = useState();
+	const [token, setToken] = useState();
+
+	const [roomDetails, setRoomDetails] = useState();
+
+	useEffect(() => {
+		const userDetails = localStorage.getItem("userInfo");
+		const token = localStorage.getItem("token");
+		if (token && userDetails) {
+			const parsedUserDetails = JSON.parse(userDetails);
+			setUserDetails(parsedUserDetails);
+			setToken(token);
+
+			getRoomDetails(token);
+		} else {
+			let message = "Please Login first.";
+			notify(message, "error");
+			setTimeout(() => {
+				navigate("/sign");
+			}, 3000);
+		}
+	}, []);
+
+	const getRoomDetails = (token) => {
+		fetch(`${roomRoutes.getRoomDetails}${roomId}`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: token,
+			},
+		})
+			.then((res) => res.json())
+			.then((res) => {
+				if (res.isError) {
+					notify(res.message, "warning");
+				} else {
+					setRoomDetails(res.room);
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+				notify(err.message, "error");
+			});
+	};
+
+	function timeConverter(inputDate) {
+		const currentDate = new Date();
+		const inputDateTime = new Date(inputDate);
+		const timeDifferenceInSeconds = Math.floor(
+			(currentDate - inputDateTime) / 1000
+		);
+
+		const rtf = new Intl.RelativeTimeFormat("en", { numeric: "auto" });
+
+		if (timeDifferenceInSeconds < 60) {
+			return rtf.format(-timeDifferenceInSeconds, "second");
+		} else if (timeDifferenceInSeconds < 3600) {
+			const minutes = Math.floor(timeDifferenceInSeconds / 60);
+			return rtf.format(-minutes, "minute");
+		} else if (timeDifferenceInSeconds < 86400) {
+			const hours = Math.floor(timeDifferenceInSeconds / 3600);
+			return rtf.format(-hours, "hour");
+		} else {
+			const options = {
+				year: "numeric",
+				month: "long",
+				day: "numeric",
+				hour: "2-digit",
+				minute: "2-digit",
+			};
+			return inputDateTime.toLocaleDateString("en-US", options);
+		}
+	}
 
 	return (
 		<div>
@@ -96,13 +245,13 @@ function Room() {
 					<p>
 						<span>Omniplex</span>{" "}
 						<span>
-							{userDetails.audio && (
+							{userState.audio && (
 								<FontAwesomeIcon icon={faMicrophone} size="2xs" />
 							)}{" "}
-							{userDetails.video && (
+							{userState.video && (
 								<FontAwesomeIcon icon={faVideo} size="2xs" />
 							)}{" "}
-							{(userDetails.audio || userDetails.video) && (
+							{(userState.audio || userState.video) && (
 								<FontAwesomeIcon
 									icon={faCircleStop}
 									style={{ color: "#e32b2b" }}
@@ -115,13 +264,13 @@ function Room() {
 						<div>
 							<button
 								onClick={() =>
-									setUserDetails((pre) => ({
+									setUserState((pre) => ({
 										...pre,
-										audio: !userDetails.audio,
+										audio: !userState.audio,
 									}))
 								}
 							>
-								{userDetails.audio ? (
+								{userState.audio ? (
 									<FontAwesomeIcon icon={faMicrophone} size="xl" />
 								) : (
 									<FontAwesomeIcon
@@ -132,26 +281,26 @@ function Room() {
 							</button>
 							<button
 								onClick={() =>
-									setUserDetails((pre) => ({
+									setUserState((pre) => ({
 										...pre,
-										video: !userDetails.video,
+										video: !userState.video,
 									}))
 								}
 							>
-								{userDetails.video ? (
+								{userState.video ? (
 									<FontAwesomeIcon icon={faVideo} size="xl" />
 								) : (
 									<FontAwesomeIcon icon={faVideoSlash} size="xl" />
 								)}
 							</button>
-							<button>
+							<button onClick={() => setDisplayChats((pre) => !pre)}>
 								<FontAwesomeIcon icon={faComment} size="xl" />
 							</button>
 							<button
 								onClick={() =>
-									setUserDetails((pre) => ({
+									setUserState((pre) => ({
 										...pre,
-										displayCode: !userDetails.displayCode,
+										displayCode: !userState.displayCode,
 									}))
 								}
 							>
@@ -159,13 +308,13 @@ function Room() {
 							</button>
 							<button
 								onClick={() =>
-									setUserDetails((pre) => ({
+									setUserState((pre) => ({
 										...pre,
-										darkMode: !userDetails.darkMode,
+										darkMode: !userState.darkMode,
 									}))
 								}
 							>
-								{userDetails.darkMode ? (
+								{userState.darkMode ? (
 									<FontAwesomeIcon icon={faMoon} size="xl" />
 								) : (
 									<FontAwesomeIcon icon={faSun} size="xl" />
@@ -184,21 +333,76 @@ function Room() {
 					</div>
 				</div>
 			</div>
+			<div style={{ position: "relative" }}>
+				{displayChats && (
+					<Draggable>
+						<div id="chat-container">
+							<div>
+								<FontAwesomeIcon
+									icon={faCircleXmark}
+									size="xl"
+									style={{
+										position: "absolute",
+										top: "-10px",
+										right: "0px",
+										zIndex:"20",
+										cursor:"pointer"
+									}}
+
+									onClick={()=>setDisplayChats(false)}
+								/>
+							</div>
+							<div id="all-chats">
+								{roomChats.length > 0 &&
+									roomChats.map((chat) => {
+										return false &&
+											chat.userId === userDetails._id ? (
+											<div id="own-update">
+												<p>{chat.message}</p>
+												<span>{timeConverter(chat.time)}</span>
+											</div>
+										) : (
+											<div>
+												<span>{chat.userName}</span>
+												<p>{chat.message}</p>
+												<span>{timeConverter(chat.time)}</span>
+											</div>
+										);
+									})}
+							</div>
+							<div>
+								<input type="text" />
+								<FontAwesomeIcon
+									icon={faPaperPlane}
+									size="lg"
+									style={{
+										position: "absolute",
+										top: "5px",
+										right: "10px",
+									}}
+								/>
+							</div>
+						</div>
+					</Draggable>
+				)}
+			</div>
+
 			<div>
-				{userDetails.displayCode ? (
+				{userState.displayCode ? (
 					<CodeEditor
-						darkMode={userDetails.darkMode}
+						darkMode={userState.darkMode}
 						isMaximized={isMaximized}
 						setIsMaximized={setIsMaximized}
 					/>
 				) : (
 					<Canvas
-						darkMode={userDetails.darkMode}
+						darkMode={userState.darkMode}
 						isMaximized={isMaximized}
 						setIsMaximized={setIsMaximized}
 					/>
 				)}
 			</div>
+			<ToastContainer />
 		</div>
 	);
 }
